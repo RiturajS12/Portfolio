3029dca7f9ab97fadb9ca97f782e3d8bd171193d
Rituraj Singh <117657890+RiturajS12@users.noreply.github.com>
2024-07-26 13:06:07 +0530

    updates

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://Textovert:Kyundu17@textovert.uzlevw3.mongodb.net/')
+db = client['upflairs'] 
+collection = db['queries']  
 
 @app.route('/')
 def index():
@@ -21,3 +26,15 @@ def certificates():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/messages', methods=['POST'])
+def submit_review():
+    name = request.form['name']
+    message = request.form['message']
+    
+    collection.insert_one({'name': name, 'message': message})
+    
+    return redirect(url_for('contact'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
