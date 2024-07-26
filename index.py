17503b73d7b0f2ec70b8909d4928ea6936c3d2a1
Rituraj Singh <117657890+RiturajS12@users.noreply.github.com>
2024-07-26 10:00:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-from wsgi import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/certificates')
+def certificates():
+    return render_template('certificate.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
